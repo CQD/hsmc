c06e294d335b52763093104a725356ba951794a6
CQD <thecqd@gmail.com>
2015-02-03 13:32:39 +0800

    更新 404.php 以符合樣板的改動

--- error/404.php
+++ error/404.php
@@ -4,15 +4,12 @@ include __DIR__ . '/../src/init.php';
 http_response_code(404);
 
 echo \Q\Core\Templator::render('main.twig', [
-    'title' => '檔案找不到',
+    'site_title' => '檔案找不到',
+    'title' => '404',
+    'desc' => '我找不到你要的檔案 /___\\',
     'head_ban' => [
        'class' => ['b2'],
        'type' => 'hero',
-       'content' => <<<CC
-404
-=================
-我找不到你要的檔案 /___\
-CC
     ],
     'content' => ''
 ]);
