b09a787c9a0244c36b3a8e081b9a6c02c9d106fc
CQD <thecqd@gmail.com>
2015-02-04 00:06:58 +0800

    加上令人感嘆時光飛逝的地動 Mod 清單

---
+++ public/mod/modlist_cat.php
@@ -0,0 +1,31 @@
+<?php
+include __DIR__ . '/../../src/init.php';
+
+echo \Q\Core\Templator::render('main.twig', [
+    'title' => '地動天驚 Mod 列表',
+    'desc' => '雖然大部分網站都爛了，還是列出來當個紀念...',
+    'main_image' => null,
+    'head_ban' => [
+       'class' => [],
+       'type' => 'sidekick',
+    ],
+    'content' => <<<MC
+
+實際上這裡差不多所有的 Mod 網站都已經不在。但是還是在此列出來，也算紀錄也算紀念...
+
+******
+
+**[Trekmods.com(原Hiigaria Planetia)](http://www.trekmods.com/index.php)**<br>
+有一些Star Trek的Mod，包括著名的[Sacrifice of Angels](http://www.trekmods.com/homeworld/soa.php)，[Innocence and Wrath](http://www.trekmods.com/homeworld/iaw.php)，[Beyond the Final Frontier](http://www.trekmods.com/cataclysm/btff.php) ，[The Wounded](http://www.trekmods.com/cataclysm/tw.php)。
+
+**[HWC: Rebirth](http://www.nickgains.com/rebirth/)**<br>
+地動專用的mod，最大的賣點是班圖西貿易船
+
+**[HWmods.de](http://www.hwmods.de/)**<br>
+這個站有"Derelicts"，"Cataclysm to Homeworld"，以及"Turanic vs Kadeshi"等三個萬艦的mod，以及地動的mod "Turanic Prowlers"。品質都相當不錯。
+
+**[Onslaught](http://www.dreamlauncher.co.uk/onslaught/)**<br>
+將許多遊戲中的單位換成萬艦和地動中未用到的船艦，品質相當不錯。
+
+MC
+]);
