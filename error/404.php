5e827416ab39f27a253be8ec7c4764a1be3aeee8
CQD <thecqd@gmail.com>
2015-02-03 14:33:46 +0800

    為 404 指定 main_image

--- error/404.php
+++ error/404.php
@@ -5,10 +5,10 @@ http_response_code(404);
 
 echo \Q\Core\Templator::render('main.twig', [
     'site_title' => '檔案找不到',
+    'main_image' => 'banner2.jpg',
     'title' => '404',
     'desc' => '我找不到你要的檔案 /___\\',
     'head_ban' => [
-       'class' => ['b2'],
        'type' => 'hero',
     ],
     'content' => ''
