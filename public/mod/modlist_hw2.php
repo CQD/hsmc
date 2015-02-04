33b816c22373b35110bbd1f21aefd5d22134ddfe
CQD <thecqd@gmail.com>
2015-02-05 02:31:42 +0800

    加上萬艦齊發二Mod列表

---
+++ public/mod/modlist_hw2.php
@@ -0,0 +1,48 @@
+<?php
+include __DIR__ . '/../../src/init.php';
+
+echo \Q\Core\Templator::render('main.twig', [
+    'title' => '萬艦齊發二Mod列表',
+    'desc' => '讓遊戲變成完全不一樣的東西',
+    'main_image' => null,
+    'head_ban' => [
+       'class' => [],
+       'type' => 'sidekick',
+    ],
+    'content' => <<<MC
+
+要尋找 Mod 最好的去處有二
+- Relicnews 討論區的 [Tanis Shipyard](http://forums.relicnews.com/forumdisplay.php?72)。有很多很多的Mod沒有自己的網頁，相關消息散落在討論串之中。而那些最活耀的Mod也都在這裡有專門的討論串。
+- ModDB 的[萬艦二專區](http://www.moddb.com/games/homeworld-2)。很多 Mod 以此為主要的散佈管道。
+
+附帶一提，差不多所有的Mod都需要把萬艦升級到1.1版後才能正常運作。
+
+以下只列出一些比較有代表性的 Mod。
+******
+
+**Complex Mod：[RN討論串](http://forums.relicnews.com/showthread.php?37486)、[網站](http://www.homeworld2complex.com/)**<br>
+這個名為「複雜」的Mod目的正是讓萬艦二變的複雜，須要更多遊戲時間，並且讓遊戲變的華麗，並增加大量的新功能與新船艦。注意，將Hw2.log檔案設成唯讀能夠大幅增進遊戲的效能。這個 Mod 的核心開發者後來有人加入了 Gearbox 的萬艦二重製小組。
+
+**Homeworld Remastered：[ModDB](http://www.moddb.com/mods/homeworld-remastered)、[RN討論串](http://forums.relicnews.com/showthread.php?261932)**<br>
+目標是把萬艦一代的戰役用二代的引擎重製，雖然 Gearbox 官方的重製版本也叫做 Homewordl Remastered 讓他們有點尷尬...
+
+**FX Mod：[ModDB](http://www.moddb.com/mods/homeworld-2-fx)、[RN討論串](http://forums.relicnews.com/showthread.php?275773)**<br>
+從大陸 9CCN 論壇（網站已隨風而逝）起家的 Mod，特點是大量的可用種族跟新船艦，以及精細的模型與動畫。
+
+**Freespace: Fleet Command v3：[ModDB](http://www.moddb.com/mods/freespace-fleet-command-v3)**<br>
+以縱橫太空(Freespace)為主題的Mod。
+
+**PDS Mod：[ModDB](http://www.moddb.com/mods/point-defense-systems)、[網站](http://www.pds.homeworldaccess.net)**<br>
+原本只是為船艦加上 Point Defense System，後來演變成子彈滿天飛的華麗戰爭。
+
+**Gundam Seed Mod：[ModDB](http://www.moddb.com/mods/homeworld-2-fx)、[RN討論串](http://forums.relicnews.com/showthread.php?104891)**<br>
+同樣是 9CCN 論壇起家的 Mod，以鋼彈 Seed 為背景
+
+**Gundam UC Mod：[ModDB](http://www.moddb.com/mods/gundam-mod-0079-to-0083)**<br>
+同樣是 9CCN 論壇起家的 Mod，時代背景為0079~0083，鋼彈迷絕對不可錯過。3.0版內附繁/簡體中文跟英文的語言檔，也可以獨立下載[繁體中文語言檔](/download/general.htm#HW2)。
+
+**Angels Fall First：[ModDB](http://www.moddb.com/mods/angels-fall-first-naval-combat-operations)、[RN討論串](http://forums.relicnews.com/showthread.php?24677)**<br>
+一個野心勃勃的原創性MOD，故事和單位皆為自創，超級華麗的原創船艦為其特點。
+
+MC
+]);
