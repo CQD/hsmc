4d1768100b5792eda3acddac0df8ef23d478d642
CQD <thecqd@gmail.com>
2015-02-05 16:11:59 +0800

    調整二代 Mod 教學的排列順序

--- public/tutorial/tutorial_hw2.php
+++ public/tutorial/tutorial_hw2.php
@@ -11,20 +11,8 @@ echo \Q\Core\Templator::render('main.twig', [
     ],
     'content' => <<<MC
 
-- [萬艦二MOD概論](/tutorial/hw2/about_modding_hw2.htm)
-- 官方文件翻譯
-  - [Big檔案工具](/tutorial/hw2/RDN_Document/HW2_ArchiveTool.htm)（ HW2_ArchiveTool ）
-  - [建造與研究腳本製作](/tutorial/hw2/RDN_Document/HW2_BuildandResearchScripting.htm)（ HW2_BuildandResearchScripting ）
-  - [遊戲模式Mod](/tutorial/hw2/RDN_Document/HW2_GameRuleMods.htm)（ HW2_GameRuleMods ）
-  - [安裝Maya Plug-in](/tutorial/hw2/RDN_Document/HW2_Maya_Install_Doc.htm)（ HW2_Maya_Install_Doc ）
-  - [Relic所使用的script語言:SCAR](/tutorial/hw2/RDN_Document/HW2_SCAR.htm)（ HW2_SCAR ）
-  - [透過.MADSTATE檔案結合模型動畫（Mesh Animation）與遊戲事件](/tutorial/hw2/RDN_Document/HW2_MadState.htm)（ HW2_MadState ）
-- Script相關教學
-  - [船隻Script內容範例說明](/tutorial/hw2/ship_script.htm)
-  - [武器修改簡例--EMP防空砲艦](/tutorial/hw2/wepn__flak_emp.htm)
-  - [如何修改單位上限](/tutorial/hw2/change_unitcap.htm)
-  - [AddAbility函式（.ship）](/tutorial/hw2/addability.htm)
 - 一般教學文章
+  - [萬艦二MOD概論](/tutorial/hw2/about_modding_hw2.htm)
   - [Mod安裝教學](/tutorial/hw2/install_mod.htm)
   - [艦徽教學](/tutorial/hw2/make_badge.htm)
   - [修改音樂和音效](/tutorial/hw2/sound.htm)
@@ -32,6 +20,18 @@ echo \Q\Core\Templator::render('main.twig', [
   - [用 Archive.exe 建立萬艦二的 Big 檔案](/tutorial/hw2/create_bigfile.htm)
   - [萬艦二所使用的檔案格式們](/tutorial/hw2/hw2_filetype.htm)
   - [地圖背景製作教學](/tutorial/hw2/background.htm)
+- Script相關教學
+  - [船隻Script內容範例說明](/tutorial/hw2/ship_script.htm)
+  - [武器修改簡例--EMP防空砲艦](/tutorial/hw2/wepn__flak_emp.htm)
+  - [如何修改單位上限](/tutorial/hw2/change_unitcap.htm)
+  - [AddAbility函式（.ship）](/tutorial/hw2/addability.htm)
+- 官方文件翻譯
+  - [Big檔案工具](/tutorial/hw2/RDN_Document/HW2_ArchiveTool.htm)（ HW2_ArchiveTool ）
+  - [建造與研究腳本製作](/tutorial/hw2/RDN_Document/HW2_BuildandResearchScripting.htm)（ HW2_BuildandResearchScripting ）
+  - [遊戲模式Mod](/tutorial/hw2/RDN_Document/HW2_GameRuleMods.htm)（ HW2_GameRuleMods ）
+  - [安裝Maya Plug-in](/tutorial/hw2/RDN_Document/HW2_Maya_Install_Doc.htm)（ HW2_Maya_Install_Doc ）
+  - [Relic所使用的script語言:SCAR](/tutorial/hw2/RDN_Document/HW2_SCAR.htm)（ HW2_SCAR ）
+  - [透過.MADSTATE檔案結合模型動畫（Mesh Animation）與遊戲事件](/tutorial/hw2/RDN_Document/HW2_MadState.htm)（ HW2_MadState ）
 
 MC
 ]);
