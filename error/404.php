cc4862ad731b01fc45d215332153a16c2921faf7
CQD <thecqd@gmail.com>
2015-02-05 23:14:24 +0800

    404 頁面加上「如果 /d 資料夾裡面有這個檔案就轉址」的 fallback

--- error/404.php
+++ error/404.php
@@ -1,6 +1,14 @@
 <?php
 include __DIR__ . '/../src/init.php';
 
+// 如果在 /d 資料夾裡面有這個檔案，redirect 過去
+$path_info = $_SERVER['PATH_INFO'];
+if (is_file(__DIR__ . "/../public/d{$path_info}")) {
+	header("Location: /d{$path_info}");
+	exit;
+}
+
+// 印出 404 頁面
 http_response_code(404);
 
 echo \Q\Core\Templator::render('main.twig', [
