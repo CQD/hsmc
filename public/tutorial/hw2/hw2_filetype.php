ea6f397bd7a3df342e0dcac2eaf8782349fbfa08
CQD <thecqd@gmail.com>
2015-02-05 19:10:38 +0800

    萬艦二檔案類型介紹

---
+++ public/tutorial/hw2/hw2_filetype.php
@@ -0,0 +1,71 @@
+<?php
+include __DIR__ . '/../../../src/init.php';
+
+echo \Q\Core\Templator::render('main.twig', [
+    'title' => '萬艦齊發二所使用的檔案格式',
+    'desc' => '這邊列出一些知道的檔案副檔名與其功能的對應',
+    'main_image' => "banner_hanger15.jpg",
+    'head_ban' => [
+       'class' => [],
+       'type' => 'sidekick',
+    ],
+    'content' => <<<MC
+
+Lua scipt檔案（純文字文件）：
+
+- .lua － 一般的lua script
+- .ti － tactical icon
+- .ship － 船隻內容
+- .subs － 子系統內容
+- .wepn － 武器內容
+- .miss － 飛彈內容
+- .cloud － 雲霧內容
+- .dustcloud － 塵雲內容
+- .nebula － 星雲內容
+- .resource － 可採集的資源內容(如可開採的小行星)
+- .level － 地圖檔
+- .events － 物件動畫／特效
+- .wf － 武器特效
+- .madstate － 模型動畫定義
+- .anim
+- .mtga
+- .lod － level-of detail
+- .mres
+- .flare
+- .stats － 遊戲總結
+- ...其他
+
+其他檔案格式：
+
+- .big,.grm － 資料壓縮檔
+- .ucs,.dat － 語系文字檔（unicode文字檔）
+- .lpr
+- .aep
+- .st
+- .vp
+- .fp
+- .ma － Maya 場景檔
+- .mad － 模型動畫
+- .hod － 船隻模型／停泊路徑／船身硬點
+- .tga － TGA 圖檔
+- .shader － 頂點著色器（Pixel shader）定義
+- .cur － 游標
+- .hotspot － 游標相關
+- .rot － Relic 的圖片格式
+- .dds － DirectX 圖片格式
+- .fda － AIFR 聲音格式(Relic專用)
+- .icon － 圖示
+- .rcf
+- .nis － 即時運算的過場動畫
+- .script
+- .sav － 遊戲存檔
+- .rec － 遊戲錄影檔（正式版中無法使用）
+- .log
+- ...其他
+
+*****
+
+[回到萬艦二 Mod 教學文件頁](/tutorial/tutorial_hw2.htm)
+
+MC
+]);
