b30286cc11d24c31b567f81369262bef4ec1572d
CQD <thecqd@gmail.com>
2015-02-12 00:09:18 +0800

    更新 sitemaps，應該要使用完整的 URL

--- gen_sitemap.sh
+++ gen_sitemap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find public -name "*.php" | sed 's/\.php$/.htm/g' | sed 's/^public\///g'
+find public -name "*.php" | sed 's/\.php$/.htm/g' | sed 's/^public\//http:\/\/hw.hiigara.net\//g'
