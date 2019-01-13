87621fa8ce1a8edab6c869c72b8471bc1d97809e
CQD <cqd@hiigara.net>
2019-01-13 23:50:23 +0800

    Add cache control

--- src/Q/HSMC/Controller/ControllerBase.php
+++ src/Q/HSMC/Controller/ControllerBase.php
@@ -8,12 +8,19 @@ use Q\Core\Templator;
 
 class ControllerBase
 {
+    protected $cacheTime = 3600;
+
     protected function render(array $data, string $template = 'main.twig')
     {
         $engine = new MarkdownEngine\MichelfMarkdownEngine();
         Templator::addExtension(new MarkdownExtension($engine));
 
-        echo Templator::render($template, $data);
+        $content = Templator::render($template, $data);
+        $checkSum = crc32($content);
+
+        header("ETag: {$checkSum}");
+        header("Cache-Control: public, max-age={$this->cacheTime}");
+        echo $content;
     }
 
     public function run(array $params = [])
