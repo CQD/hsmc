fe212fb32311b51384c53538c45a5f9e1e57395e
CQD <cqd@hiigara.net>
2019-01-14 00:01:45 +0800

    gen_sitemap.sh 跟現況已不吻合，拔之

--- gen_sitemap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find public -name "*.php" \
-    | sed 's/\.php$/.htm/g' \
-    | sed 's/^public\//http:\/\/hw.hiigara.net\//g' \
-    | grep -v '/index\.htm\|/home\.htm\|cqd\.htm' \
-    | sort
