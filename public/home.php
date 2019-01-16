537e6a107ab04770dc6cef8886327aee79ec8a9a
CQD <cqd@hiigara.net>
2019-01-16 20:46:28 +0800

    補完首頁的遊戲歷史

--- public/home.php
+++ public/home.php
@@ -13,16 +13,20 @@ return [
 1999 年 [Relic Entertainment](http://www.relic.com) 推出的萬艦齊發(Homeworld)是一款全立體的太空戰爭戰略遊戲。玩家指揮手上的宇宙艦隊，不是侷限在 2D 平面上，而是在真正的開放空間中立體機動，彼此戰鬥。
 從今天的角度來看，這款遊戲的畫面或許已經過時，但這是第一次玩家可以在自己的螢幕上，用一切角度掌握一場如同電影畫面的宇宙戰爭。
 
-萬艦齊發除了畫面驚人，劇情也十分出色。從一代庫沈人賭上種族的存亡，離開沙漠星球卡拉克，回到遠離千年的家園西亞加拉，到資料片(2000)面對無法擊敗的敵人，
-再到二代(2003)面對一場再次奪走他們家園的戰爭。即使過了這麼多年，也依然讓人難以忘懷。
+萬艦齊發除了畫面驚人，劇情也十分出色。從一代庫沈人賭上種族的存亡，離開沙漠星球卡拉克，回到遠離千年的家園西亞加拉，到資料片地動天驚(Homeworld: Cataclysm, 2000)面對無法擊敗的敵人，
+再到二代(Homeworld 2, 2003)面對一場再次奪走他們家園的戰爭。即使過了這麼多年，也依然讓人難以忘懷。
 
 ----
 
-到了戲外，萬艦齊發這塊商標本身也跟遊戲劇情一樣命運坎坷。在延宕多年的二代上市後，萬艦齊發隨著 Relic 由原本的發行商 [Sierra](http://www.sierra.com/) 被轉移給新東家 [THQ](http://www.thq.com)。然而 Relic 還沒有機會推出萬艦齊發的後續作品，就在 2013 年的 THQ 破產拍賣中被賣給了 Sega，萬艦齊發則是被賣給了 [Gearbox Software](http://www.gearboxsoftware.com/)。
+到了戲外，萬艦齊發這塊商標本身也跟遊戲劇情一樣命運坎坷。在延宕多年的二代上市後，萬艦齊發隨著 Relic 由原本的發行商 [Sierra](http://www.sierra.com/) 被轉移給新東家 [THQ](http://www.thq.com)。然而 Relic 再也沒有機會推出萬艦齊發的後續作品。
 
-2015 年，在 12 年的空窗期之後，萬艦齊發的重製版終於上市。而前傳萬艦齊發之卡拉克荒漠（Homewolrd: Desert of Kharak）也在 2016 年初推出。
+一部分萬艦齊發原本的開發者成立了新公司 [Black Bird Interactive](https://blackbirdinteractive.com/)，2010 起嘗試開發以地面戰爭為主題的精神續作 Hardware: Shipbreakers。當時遊戲預計會以 Free 2 Play 的線上對戰形式推出。
 
-不論遊戲內遊戲外，這都能算是遊戲史上最精彩的宇宙戰爭故事之一。
+2013 年 THQ 宣告破產，在破產大拍賣中 Relic 工作室被賣給了 Sega，萬艦齊發則是被賣給了 [Gearbox Software](http://www.gearboxsoftware.com/)，從此萬艦齊發與原本的開發公司分道揚鑣。而擅長製作 FPS 的 GearBox 決定提供資金以及萬艦齊發的商標給 Black Bird Interactive，讓他們能夠開發真正的萬艦齊發續作。
+
+2015 年，在 12 年的空窗期之後，[萬艦齊發重製版](https://store.steampowered.com/app/244160)終於上市。而蛻變自 Hardware: Shipbreakers 的萬艦齊發前傳，[萬艦齊發之卡拉克荒漠（Homewolrd: Desert of Kharak）](https://store.steampowered.com/app/281610)也在 2016 年初推出。交由 Barking Dog 開發的地動天驚原本以為在 Rockstar 收購 Barking Dog 工作室時遺失了所有的程式原始資料，但也意外的在 2017 年以 [Homeworld: Emergence](https://www.gog.com/game/homeworld_emergence) 的名義在 GOG 上架。
+
+不論遊戲內遊戲外，這都是遊戲史上值得記上一筆的宇宙戰爭故事。
 
 MC
 ];
