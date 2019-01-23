42cc2154788d773cc921c95fa8e422bcd498ccc8
CQD <cqd@hiigara.net>
2019-01-23 19:00:03 +0800

    拔掉 Templator

--- src/Q/HSMC/Controller/ControllerBase.php
+++ src/Q/HSMC/Controller/ControllerBase.php
@@ -4,18 +4,27 @@ namespace Q\HSMC\Controller;
 
 use Aptoma\Twig\Extension\MarkdownExtension;
 use Aptoma\Twig\Extension\MarkdownEngine;
-use Q\Core\Templator;
 
 class ControllerBase
 {
     protected $cacheTime = 3600;
 
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../../../../twig');
+        $this->twig = new \Twig_Environment($loader);
+
+        $markdownEngine = new MarkdownEngine\MichelfMarkdownEngine();
+        $markdownExtension = new MarkdownExtension($markdownEngine);
+        $this->twig->addExtension($markdownExtension);
+    }
+
+
     protected function render(array $data, string $template = 'main.twig')
     {
-        $engine = new MarkdownEngine\MichelfMarkdownEngine();
-        Templator::addExtension(new MarkdownExtension($engine));
-
-        $content = Templator::render($template, $data);
+        $content = $this->twig->render($template, $data);
         $checkSum = crc32($content);
 
         header("ETag: {$checkSum}");
