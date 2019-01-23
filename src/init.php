73e9f482ae3f9b02b59661765d8f3b035b479fce
CQD <cqd@hiigara.net>
2019-01-23 19:01:19 +0800

    拔掉沒有在用的 code

--- src/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-include __DIR__ . '/../vendor/autoload.php';
-
-use Aptoma\Twig\Extension\MarkdownExtension;
-use Aptoma\Twig\Extension\MarkdownEngine;
-
-$engine = new MarkdownEngine\MichelfMarkdownEngine();
-\Q\Core\Templator::addExtension(new MarkdownExtension($engine));
