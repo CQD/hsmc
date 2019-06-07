9358a8e21fe41152d0bc42e68f79e83eed6f1f9c
CQD <cqd@hiigara.net>
2019-06-07 22:02:36 +0800

    移除 jquery 跟 modernizr

--- public/j/main.js
+++ public/j/main.js
@@ -1,19 +1,45 @@
-$(function(){
-	$('nav>ul>li').on({
-		mouseover: function(){
-			$('nav .active').removeClass('active');
-		},
-		touchstart: function(){
-			var $me = $(this);
-			if ($me.hasClass('active')) {
-				$me.removeClass('active');
-			} else {
-				$('nav .active').removeClass('active');
-				$(this).addClass('active');
-			}
-		}
-	});
-	$('html').on('click', function(){
-		$('nav .active').removeClass('active');
-	})
-});
+function e(elements)
+{
+    if (typeof(elements) === 'string') {
+        elements = document.querySelectorAll(elements)
+    } else if (elements.tagName) {
+        elements = [elements];
+    }
+
+    return elements || [];
+}
+
+function removeClass(elements, className)
+{
+    e(elements).forEach((ele) => {
+        let reg = new RegExp(`(^| )${className}($| )`);
+        ele.className = ele.className.replace(reg, ' ').trim()
+    });
+}
+
+function addClass(elements, className)
+{
+    e(elements).forEach((ele) => {
+        ele.className = `${ele.className} ${className}`
+    });
+}
+
+document.querySelectorAll('nav>ul>li').forEach(function(ele){
+    ele.addEventListener('mouseover', function(e){
+        removeClass('nav .active', 'active');
+    });
+
+    ele.addEventListener('touchstart', function(e){
+        let reg = new RegExp(`(^| )active($| )`);
+        if (this.className.match(reg)) {
+            removeClass(this, 'active');
+        } else {
+            removeClass('nav .active', 'active');
+            addClass(this, 'active')
+        }
+    });
+})
+
+e('html')[0].addEventListener('click', function(){
+    removeClass('nav .active', 'active')
+})
