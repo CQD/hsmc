415ddd6f88b9e599a7af54e748aa43c61598ebbb
CQD <cqd@hiigara.net>
2023-04-12 21:30:17 +0800

    更新連結。歷史又翻過了一頁...

--- public/link.php
+++ public/link.php
@@ -9,34 +9,30 @@ return [
     ],
     'content' => <<<MC
 
-- Steam 上的[萬艦齊發重製版](https://store.steampowered.com/app/244160)商店頁面
-- GoG 上的[地動天驚](https://www.gog.com/game/homeworld_emergence)商店頁面
+- Steam 上的[萬艦齊發重製版](https://store.steampowered.com/app/244160) / [卡拉克荒漠](https://store.steampowered.com/app/281610) / [萬艦齊發三](https://store.steampowered.com/app/1840080)。
+- GoG 上的[地動天驚](https://www.gog.com/game/homeworld_emergence)。
 
 ----------------------
 
-- Gearbox 的[萬艦重製版討論區](http://forums.gearboxsoftware.com/c/homeworld)
-- Blackbird Interactive  的[卡拉克荒漠討論區](http://forums.blackbirdinteractive.com/forum/deserts-of-kharak)
+- [Gearbox Software](https://www.gearboxsoftware.com/)<br>
+  萬艦齊發目前代理商。
+- [Blackbird Interactive](https://blackbirdinteractive.com/)<br>
+  由部分原本萬艦齊發老將組成的工作室，卡拉克荒漠與萬艦齊發三的開發商。
+- [Relic](https://www.relic.com/)<br>
+  萬艦齊發一代與二代的開發商。
+
+----------------------
+
 - [Homeworld Universe@Discord](https://discordapp.com/channels/128196211969753088)<br>
   萬艦齊發的 Discord 頻道。
-- [Fists of Heaven](http://www.fistsofheaven.com/)<br>
+- [Fists of Heaven](https://www.fistsofheaven.com/)<br>
   2013 年才成立的萬艦齊發粉絲網站，有粉絲創作、人物訪談、甚至是其他遊戲的評鑑。
-- [Homeworld Shipyard](http://shipyards.relicnews.com/)<br>
+- [Homeworld Shipyard](https://well-of-souls.com/homeworld/)<br>
   介紹萬艦系列的各種船艦。
-- [Encyclopedia Hiigara](http://homeworld.wikia.com)<br>
+- [Encyclopedia Hiigara](https://homeworld.wikia.com)<br>
   西亞加拉大百科是萬艦系列主題 Wiki
-- [KarosGraveyard](http://hw2wiki.net/wiki.hw2.info/KarosGraveyard.html)<br>
-  萬艦 Mod 技術主題 Wiki
-- ModDB 的[萬艦一代](http://www.moddb.com/games/homeworld)、[萬艦二代](http://www.moddb.com/games/homeworld-2)、[萬艦重製版](https://www.moddb.com/games/homeworld-remastered)專區
-- 深度時空論壇的[家園討論板](http://bbs.deeptimes.org/forum.php?mod=forumdisplay&fid=42)與[泰坦帝國](http://www.taiidan.net)<br>
+- 深度時空論壇的[家園討論板](https://bbs.deeptimes.org/forum.php?mod=forumdisplay&fid=42)與[泰坦帝國](https://www.taiidan.net)<br>
   大陸的萬艦齊發討論區。過去還有過 9CCN、《家園》官方論壇...等大型集散地，但不少論壇都已經陣亡。
-- [巴哈姆特萬艦齊發討論板](http://forum.gamer.com.tw/A.php?bsn=01029)
-- [遊戲基地萬艦齊發討論板](http://www.gamebase.com.tw/forum/1535)
-- [The Gaming Union討論版](http://www.thegamingunion.co.uk/forums/)<br>
-  在萬艦齊發一代有不少老字號的MOD將討論版設在此處。例如Star Trek， Star Wars， B5.....等等。目前網站已經停止運作。
-- [Relicnews](http://www.relicnews.com) 與其[討論區](http://forums.relicnews.com)<br>
-  追蹤 Relic 工作室最新消息的資訊網站，討論區有歷史悠久的愛好者社群，曾經是最大的萬艦齊發資訊聚集地。目前網站已經停止運作。
-- [西亞加拉年代記](http://www.geocities.jp/hiigara_chronicle/)<br>
-  難得的日文網站，曾經在自炊日文化，已經停頓
-
+- [巴哈姆特萬艦齊發討論板](https://forum.gamer.com.tw/A.php?bsn=01029)
 MC
 ];
